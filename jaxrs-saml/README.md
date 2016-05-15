61d55de2c1cbbc1bd777683ca022449a4f81d4d8
Andrei Shakirin <andrei.shakirin@gmail.com>
2016-05-15 20:46:24 +0200

    Added STS Token validation option

--- jaxrs-saml/README.md
+++ jaxrs-saml/README.md
@@ -9,6 +9,9 @@
 3. in jaxrs-bookstore project
    3.1) start jax-rs service:
         mvn exec:java -Dexec.mainClass="demo.jaxrs.saml.server.BookstoreServerSpring"
+   
+        alternatively to use STS token validation start service with:
+        mvn exec:java -Dexec.mainClass="demo.jaxrs.saml.server.BookstoreServerSpringSTSValidation"
 
    3.2) start jax-rs client:
         mvn exec:java -Dexec.mainClass="demo.jaxrs.saml.client.BookstoreClientSpring"
--- jaxrs-saml/jaxrs-bookstore/src/main/java/demo/jaxrs/saml/server/BookstoreServerSpringSTSValidation.java
+++ jaxrs-saml/jaxrs-bookstore/src/main/java/demo/jaxrs/saml/server/BookstoreServerSpringSTSValidation.java
@@ -3,14 +3,14 @@ package demo.jaxrs.saml.server;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 
-public class BookstoreServerSpring {
+public class BookstoreServerSpringSTSValidation {
 
 	public static void main(String argv[]) throws InterruptedException {
 		
         // Initialize the spring context and fetch our test client
 		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
-				new String[] { "classpath:server-context.xml" });
-        Thread.sleep(100000000);
+				new String[] { "classpath:server-context-sts-validation.xml" });
+	       Thread.sleep(100000000);
         
         System.out.println("Shutdown server ...");
         context.close();
